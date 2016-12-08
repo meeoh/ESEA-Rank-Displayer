30c800f33bd2e7858a4809229fa85bce6eb72805
Shameel Abdullah <shameel.abdullah@gmail.com>
2016-12-08 16:57:42 -0500

    fully done

--- content_script.js
+++ content_script.js
@@ -1,1 +1,15 @@
-console.log("TEST")
+function findRanks(i) {
+    var allUsers = $(document).find("#body-match-total" + i + " tr");   
+
+    $.each($(document).find("#body-match-total" + i + " tr"), function(index, value){
+        var userLink = "https://play.esea.net/users/" + allUsers[index].children[0].children[1].innerHTML
+        $.get(userLink, function(data) {
+            var parsed = $('<div/>').append(data);
+            rank = $(parsed).find("#rankGraph h1").text();
+            allUsers[index].children[0].children[1].innerHTML += " (" + rank + ")";           
+        });
+    });
+}
+
+findRanks(1);
+findRanks(2)
