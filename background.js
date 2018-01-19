4e9327126e3b00ddc0d27464925e1308282003d8
Shameel Abdullah <shameel.abdullah@esentire.com>
2018-01-19 15:30:55 -0500

    works without refresh

--- background.js
+++ background.js
@@ -1,2 +1,16 @@
 //Background.js
 //background script for ESEA Rank Displayer
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+    if (details.url.match("https://play.esea.net/match/*")) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function(
+            tabs
+        ) {
+            chrome.tabs.sendMessage(
+                tabs[0].id,
+                { action: "refreshRanks" },
+                function(response) {}
+            );
+        });
+    }
+    // do your thing
+});
